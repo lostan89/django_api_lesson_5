27d02b6cda4d0d6f20151da2042f2953ee578f4f
lostan89 <skeeperenator@gmail.com>
2026-02-23 19:20:34 +0300

    update 7: fix code

--- main.py
+++ main.py
@@ -35,12 +35,11 @@ def fetch_hh_vacancies(profession):
 
         response.raise_for_status()
         vacancies = response.json()
-        collected_vacancies.append(vacancies)
-
-        if page > collected_vacancies[0]["pages"]:
+        vacancies_found = vacancies.get("found",0)
+        collected_vacancies.extend(vacancies.get('items'))
+        if page >= vacancies.get("pages")-1:
             break
-
-    return collected_vacancies
+    return collected_vacancies, vacancies_found
 
 
 def fetch_superjob_vacancies(profession, secret_key):
@@ -114,18 +113,16 @@ def get_hh_salary_stats():
     average_salaries_by_vacancy = {}
 
     for profession in PROFESSIONS:
-        collected_vacancies = fetch_hh_vacancies(profession)
-        vacancies_found = collected_vacancies[0]["found"]
+        collected_vacancies, vacancies_found = fetch_hh_vacancies(profession)
         rub_salaries = []
-        for page_of_vacancies in collected_vacancies:
-            for vacancy in page_of_vacancies["items"]:
-                if not vacancy["salary"]:
-                    continue
-                if vacancy["salary"]["currency"] == "RUR":
-                    salary = predict_rub_salary(
-                        vacancy["salary"]["from"], vacancy["salary"]["to"]
-                    )
-                    rub_salaries.append(int(salary))
+        for vacancy in collected_vacancies:  
+            if not vacancy["salary"]:
+                continue
+            if vacancy["salary"]["currency"] == "RUR":
+                salary = predict_rub_salary(
+                    vacancy["salary"]["from"], vacancy["salary"]["to"]
+                )
+                rub_salaries.append(int(salary))
         vacancies_processed = len(rub_salaries)
         if vacancies_processed > 0:
             average_salary = sum(rub_salaries) / vacancies_processed
